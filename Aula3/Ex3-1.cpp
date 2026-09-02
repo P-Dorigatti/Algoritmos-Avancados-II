7d8863fef605a3f9e9cb6a6179b54cc409996a6a
P-Dorigatti <domgamerraduken@gmail.com>
2026-09-02 14:38:49 -0300

    resolvendo exponenciacao rapida

--- Aula3/Ex3-1.cpp
+++ Aula3/Ex3-1.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <list>
-#include <map>
-#include <cstdio>
 
 using namespace std;
+
+const long long MOD = 1000000007;
+
+long long power_modulo(long long a, long long b) {
+    long long resultado = 1;
+    
+    a = a % MOD; 
+
+    while (b > 0) {
+        if (b % 2 == 1) {
+            resultado = (resultado * a) % MOD;
+        }
+        
+        a = (a * a) % MOD;
+        b = b / 2;
+    }
+    
+    return resultado;
+}
+
+int main() {
+
+    int t;
+    cin >> t;
+    for(int i = 0; i < t; i++){
+        long long a, b;
+        cin >> a >> b;
+        cout << power_modulo(a, b) << "\n";
+    }
+
+    return 0;
+}
